7bfa22309015c575811ce2c7e03b588a8b29035b
kimsinha11 <tlsgk2013@gmail.com>
2023-03-21 13:40:25 +0900

    게시물 작성시 DB에 INSERT

--- src/com/KoreaIT/example/JAM/Main.java
+++ src/com/KoreaIT/example/JAM/Main.java
@@ -1,4 +1,8 @@
 package com.KoreaIT.example.JAM;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -30,7 +34,9 @@ public class Main {
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
 				String regDate = "";
-
+				
+				
+				
 				Article article = new Article(id, title, body, regDate);
 				articles.add(article);
 				
@@ -38,7 +44,48 @@ public class Main {
 				System.out.printf("%d / %s \n", article.id, article.title);
 
 				System.out.printf("%d번 글이 생성되었습니다.\n", id);
+				
+				Connection conn = null;
+				PreparedStatement pstmt = null;
+				try {
+					Class.forName("com.mysql.jdbc.Driver");
+					String url = "jdbc:mysql://127.0.0.1:3306/JAM?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+					
+					conn = DriverManager.getConnection(url, "root", "");
+					System.out.println("연결 성공!");
+					
+					String sql = "INSERT INTO article";
+					sql += " SET regDate = NOW(),";
+					sql += "updateDate = NOW(),";
+					sql += "title = '" + title + "',";
+					sql += "`body` = '" + body + "';";
+					
+					System.out.println(sql);
+					
+					pstmt = conn.prepareStatement(sql);
+					
+					int affectedRow = pstmt.executeUpdate();
+					
+					System.out.println("affectedRow : " + affectedRow);
+					
+				} catch (ClassNotFoundException e) {
+					System.out.println("드라이버 로딩 실패");
+					
+				} catch (SQLException e) {
+					System.out.println("에러 : " + e);
+					
+				} finally {
+					try {
+						if (pstmt != null && !pstmt.isClosed()) {
+							pstmt.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+				
 				lastArticleId++;
+				
 			} else if (cmd.equals("article list")) {
 				System.out.println("==게시물 리스트==");
 				if (articles.size() == 0) {
